ba6c833ccba1b6b511f2af666f578ecf35fdd0b0
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-02 11:20:15 -0500

    Add plugin support for updates and single-instance handling
    Integrated `tauri_plugin_updater` for managing application updates with download and install functionality. Added `tauri_plugin_single_instance` to ensure single-instance behavior and improved focus management. Refactored and reorganized Windows key hook initialization to align with new features.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,20 +1,35 @@
 use std::thread;
+use tauri_plugin_updater::UpdaterExt;
 
 mod windows_api;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    thread::spawn(|| {
-        println!("Starting Windows key hook...");
-        windows_api::block_windows_key();
-    });
-
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
+            use tauri::Manager;
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }))
         .setup(|app| {
             #[cfg(desktop)]
             {
                 use tauri_plugin_autostart::MacosLauncher;
                 use tauri_plugin_autostart::ManagerExt;
+
+                let handle = app.handle().clone();
+                tauri::async_runtime::spawn(async move {
+                    update(handle).await.unwrap();
+                });
+
+
+                thread::spawn(|| {
+                    println!("Starting Windows key hook...");
+                    windows_api::block_windows_key();
+                });
 
                 app.handle().plugin(tauri_plugin_autostart::init(
                     MacosLauncher::LaunchAgent,
@@ -38,4 +53,29 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+
+    if let Some(update) = app.updater()?.check().await? {
+        let mut downloaded = 0;
+
+        // alternatively we could also call update.download() and update.install() separately
+        update
+            .download_and_install(
+                |chunk_length, content_length| {
+                    downloaded += chunk_length;
+                    println!("downloaded {downloaded} from {content_length:?}");
+                },
+                || {
+                    println!("download finished");
+                },
+            )
+            .await?;
+
+        println!("update installed");
+        app.restart();
+    }
+
+    Ok(())
 }
